b94cafef9a48186e8be106ec003754a5729a2a2d
Hemanthhh <hemanthvarma995@gmail.com>
2025-02-08 13:15:15 -0600

    Added Work page CSS

--- Portfolio/src/components/Work/Work.jsx
+++ Portfolio/src/components/Work/Work.jsx
@@ -8,34 +8,43 @@ function Work() {
             "company": "U.S. Bank",
             "startDate": "Aug 2022",
             "endDate": "Present",
+            "role": "Full Stack Developer"
         },
         {
             "id": 2,
             "company": "Microsoft",
             "startDate": "Mar 2022",
             "endDate": "Aug 2022",
+            "role": "Software Development Engineer"
         },
         {
             "id": 3,
             "company": "OCM Advisory Group",
             "startDate": "Sept 2021",
             "endDate": "Dec 2021",
+            "role": "Software Engineer"
         },
         {
             "id": 4,
             "company": "SharkNinja",
             "startDate": "Mar 2021",
             "endDate": "Sept 2021",
+            "role": "Software Engineer - Data Platform"
         }
     ];
 
     return (
         <div className="Work">
-            <ul className="Work__list">
+            <h1 className="Title">Work</h1>
+            <ul >
                 {workExperience.map((item) => (
-                    <ul>
-                        <li key={item.id}>{item.startDate + "-" + item.endDate}</li>
-                        <li key={item.id}>{item.company}</li>
+                    <ul className="WorkList">
+                        <li key={item.id} className="WorkListItem">{item.startDate + "-" + item.endDate}</li>
+                        <li key={item.id} className="WorkListItem">
+                            {item.company}
+                            <br/>
+                            {item.role}
+                        </li>
                     </ul>
                 ))}
             </ul>
