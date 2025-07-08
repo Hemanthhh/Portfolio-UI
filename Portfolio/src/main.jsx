6dfcc32d81b237a3049007b787333a7763f685ad
Hemanthhh <hemanthvarma995@gmail.com>
2025-07-07 20:39:36 -0500

    Refactor CSS formatting and enhance main application structure by adding SocialMedia component

--- Portfolio/src/main.jsx
+++ Portfolio/src/main.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
-import {createRoot} from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import Navbar from './components/Navbar/Navbar'
 import Intro from './components/Intro/Intro'
 import Experience from './components/Experience/Experience'
 import Skills from './components/Skills/Skills'
+import SocialMedia from './components/SocialMedia/SocialMedia'
 
 const App = () => {
     return (
         <React.StrictMode>
-            <Navbar/>
-            <Intro/>
-            <Experience/>
-            <Skills/>
+            <Navbar />
+            <Intro />
+            <Experience />
+            <Skills />
+            <SocialMedia />
         </React.StrictMode>
     )
 }
 
 const container = document.getElementById('root')
 const root = createRoot(container);
-root.render(<App/>)
+root.render(<App />)
